cc173ec94725527d7f9855736ae3680d9dd538dc
Pedro Henrique Pontes Martins <phpontesmartins19@gmail.com>
2026-07-02 22:50:37 -0300

    :sparkles: feat: add the base structure of the desktop project

---
+++ app/desktop/src/renderer/src/main.jsx
@@ -0,0 +1,11 @@
+import './assets/main.css'
+import { HashRouter } from "react-router-dom";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
+
+createRoot(document.getElementById('root')).render(
+  <HashRouter>
+    <App />
+  </HashRouter>
+)
