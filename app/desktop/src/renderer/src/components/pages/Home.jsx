091adfbc0b6729fd15f6697fbc7baf20e041a507
Pedro Henrique Pontes Martins <phpontesmartins19@gmail.com>
2026-07-10 18:03:08 -0300

    :sparkles: feat: add structure of the Home page

--- app/desktop/src/renderer/src/components/pages/Home.jsx
+++ app/desktop/src/renderer/src/components/pages/Home.jsx
@@ -1,8 +1,27 @@
+import { useNavigate } from 'react-router-dom'
+
 import Header from '../layout/Header.jsx'
+import Footer from '../layout/Footer.jsx'
+
+import homeImg from '../../assets/cellphoneAndPC-Image.png'
+
 function Home(){
+    const navigate = useNavigate()
+
     return (
         <>
             <Header/>
+            
+            <main>
+                <img src={homeImg} alt="Image of a cellphone and computer" />
+
+                <div>
+                    <button onClick={() => {navigate("/MirrorScreen")}}>Mirror Screen</button>
+                    <button onClick={() => {navigate("/ConnectDevice")}}>Connect Device</button>
+                </div>
+            </main>
+
+            <Footer/>
         </>
     )
 }
