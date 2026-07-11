47a25a1b8f1d53c1b205354668fcc5304328cefb
Pedro Henrique Pontes Martins <phpontesmartins19@gmail.com>
2026-07-11 15:29:45 -0300

    :lipstick: feat: add style to the connection page

--- app/desktop/src/renderer/src/components/pages/ConnectionPage.jsx
+++ app/desktop/src/renderer/src/components/pages/ConnectionPage.jsx
@@ -5,20 +5,26 @@ import Footer from '../layout/Footer.jsx'
 
 import seta from '../../assets/seta-esquerda.png'
 
+import styles from './ConnectionPage.module.css'
+
 function ConnectionPage(){
 
     return(
         <>
             <Header/>
-                <a href="/">
-                    <img src={seta} alt="Arrow. Come back to the home page." />
+            
+            <main className={styles.main}>
+                <a href="/" className={styles.link}>
+                    <img src={seta} alt="Arrow. Come back to the home page." className={styles.img}/>
                 </a>
 
-                <div>
-                    <label htmlFor="code">Enter the connection code:</label>
-                    <input type="text" id='code'/>
-                    <button>Connect device</button>
+                <div className={styles.formsContainer}>
+                    <label htmlFor="code" className={styles.label}>Enter the connection code:</label>
+                    <input type="text" id='code' className={styles.input}/>
+                    <button className={styles.button}>Connect device</button>
                 </div>
+            </main>
+
             <Footer/>
         </>
     )
