3b745234f4e235e176a52e02ba5b8d482c765a6d
Pedro Henrique Pontes Martins <phpontesmartins19@gmail.com>
2026-07-23 20:17:02 -0300

    :sparkles: feat: add the structure and the route to the Stream Page

---
+++ app/desktop/src/renderer/src/components/pages/StreamPage.jsx
@@ -0,0 +1,13 @@
+import power from '../../assets/power-icon.svg'
+
+function StreamPage(){
+    
+    return(
+        <>
+            <button><img src={power} alt="power button" /></button>
+            <video src=""></video>
+        </>
+    )
+}
+
+export default StreamPage
