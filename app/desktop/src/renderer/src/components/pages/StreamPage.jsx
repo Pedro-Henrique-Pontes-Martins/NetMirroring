e0d65b8be169f4859ff157cc6fd134b070bab137
Pedro Henrique Pontes Martins <phpontesmartins19@gmail.com>
2026-07-23 21:39:02 -0300

    :lipstick: feat: add the style to the Stream Page

--- app/desktop/src/renderer/src/components/pages/StreamPage.jsx
+++ app/desktop/src/renderer/src/components/pages/StreamPage.jsx
@@ -1,11 +1,13 @@
 import power from '../../assets/power-icon.svg'
 
+import styles from './StreamPage.module.css'
+
 function StreamPage(){
     
     return(
         <>
-            <button><img src={power} alt="power button" /></button>
-            <video src=""></video>
+            <button className={styles.button}><img src={power} className={styles.svg} alt="power button" /></button>
+            <video src="" className={styles.video}></video>
         </>
     )
 }
